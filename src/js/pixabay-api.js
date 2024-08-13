276ef7a7f9aab576484ed612826351762100299c
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-08-13 17:57:27 +0200

    renewed pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = '45272920-c2160489642e002ce9de87f86';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
+export async function fetchImages(query, page = 1) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
     query
-  )}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const response = await fetch(url);
-  if (!response.ok) {
+  )}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`;
+  const response = await axios.get(url);
+  if (response.status !== 200) {
     throw new Error('Network response was not ok');
   }
-  return response.json();
+  return response.data;
 }
